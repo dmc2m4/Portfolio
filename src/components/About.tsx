23e9cee5d5a40d47f5355d1bd00e67967cc695d3
dmc2m4 <111526319+dmc2m4@users.noreply.github.com>
2023-02-27 12:20:57 -0500

    solucionando responsive para movil

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,19 +6,19 @@ const About = () => {
                 <div className='pb-8'>
                     <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
                 </div>
-                <p className='text-l mt-8 sm:text-xl'>
+                <p className='text-l sm:text-xl'>
                 Full Stack Developer with experience in technologies such as JavaScript, React JS, Node JS, Express JS and PostgresSQL. I’m interested in continuing learning new technologies and obtain skills that make me not only a better professional but also a better person.
                 </p>
                 <br/>
-                <p className='text-l sm:text-xl'>
+                <p className='text-m sm:text-xl'>
                 My main skills are the ability to analyze and solve problems, willingness to work in team and to learn.
                 </p>
                 <br/>
-                <p className='text-l sm:text-xl'>
+                <p className='text-m sm:text-xl'>
                 I have developed two projects as a programmer in which I used technologies listed before, and thanks to this I have developed personal skills such as leadership, teamwork and adaptability.
                 </p>
                 <br/>
-                <p className='text-l mb-10 sm:text-xl'>
+                <p className='text-m mb-10 sm:text-xl'>
                 I am a mechanical engineer graduate from Universidad del Norte. Since then I have gained work experience in companies such as CocaCola FEMSA in maintenance area, also worked as freelancer in sectors such as stock market, administrative positions and logistics of mass events.
                 </p>
             </div>
